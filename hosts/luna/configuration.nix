c1301d9e23e801d7ded5d5d0ba04bd434c024699
Luna Skye <sepshuncontact@gmail.com>
2024-10-18 22:20:23 -0400

    host(luna): refactor session attr structure

--- hosts/luna/configuration.nix
+++ hosts/luna/configuration.nix
@@ -3,24 +3,28 @@
 
   bead = {
     host.name = "luna";
-    session.hyprland = {
-      enable = true;
-      monitors = [
-        {
-          name = "DP-1";
-          resolution = "2440x1440";
-          offset = "0x0";
-          refreshRate = 165;
-          workspaces = [1 2 3 4 5];
-        }
-        {
-          name = "HDMI-A-1";
-          resolution = "1920x1080";
-          offset = "2560x360";
-          refreshRate = 60;
-          workspaces = [6 7 8 9];
-        }
-      ];
+
+    session = {
+      default = "hyprland";
+      hyprland = {
+        enable = true;
+        monitors = [
+          {
+            name        = "DP-1";
+            resolution  = "2440x1440";
+            offset      = "0x0";
+            refreshRate = 165;
+            workspaces  = [ 1 2 3 4 5 ];
+          }
+          {
+            name        = "HDMI-A-1";
+            resolution  = "1920x1080";
+            offset      = "2560x360";
+            refreshRate = 60;
+            workspaces  = [ 6 7 8 9 ];
+          }
+        ];
+      };
     };
   };
 
