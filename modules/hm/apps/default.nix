ad18db86d2117d79ed8f4352266f7262d5d6d6f0
Luna Skye <sepshuncontact@gmail.com>
2024-10-19 03:24:14 -0400

    refactor(hm/apps): move default browser opt to app scope
    should allow for flatter apps attrset when configuring

--- modules/hm/apps/default.nix
+++ modules/hm/apps/default.nix
@@ -1,3 +1,40 @@
-{ bead, ... }: {
+{ config, lib, bead, ... }: let
+  cfg = config.bead.apps;
+in {
   imports = bead.autoload ../apps;
+
+
+  options.bead.apps = {
+    default = {
+      browser = bead.mkListOfOption lib.types.str [] "User's default browser application";
+    };
+  };
+
+
+  config = let
+    browserAssociations = builtins.listToAttrs(map (s: { name = s; value = cfg.default.browser; }) [
+      "text/html"
+      "x-scheme-handler/http"
+      "x-scheme-handler/https"
+      "x-scheme-handler/ftp"
+      "x-scheme-handler/about"
+      "x-scheme-handler/unknown"
+      "application/x-extension-htm"
+      "application/x-extension-html"
+      "application/x-extension-shtml"
+      "application/xhtml+xml"
+      "application/x-extension-xhtml"
+      "application/x-extension-xht"
+      "application/json"
+      "application/pdf"
+    ]);
+
+    xdgAssociations = {
+    } // browserAssociations;
+  in {
+    xdg.mimeApps = {
+      associations.added = xdgAssociations;
+      defaultApplications = xdgAssociations;
+    };
+  };
 }
