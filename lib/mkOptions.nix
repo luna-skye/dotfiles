6ac9a7e248ed089371da937512732adcd310c4e6
Luna Skye <sepshuncontact@gmail.com>
2024-10-19 16:45:48 -0400

    refactor(lib): add mkPackageOption function
    allows easily creating package options with an enable bool option and an overridable default pkg

--- lib/mkOptions.nix
+++ lib/mkOptions.nix
@@ -1,6 +1,6 @@
 { inputs, ... }: let
   inherit (inputs.nixpkgs) lib;
-in {
+in rec {
   # Creates a Nix Option of the boolean type, with a passed doc description and default value
   mkBooleanOption = default: desc: lib.mkOption {
     description = desc;
@@ -41,5 +41,15 @@ in {
     description = desc;
     type = lib.types.nullOr type;
     default = default;
+  };
+
+  # Creates a nested option set for enabling and overriding a Nix package
+  mkPackageOption = pkg: name: {
+    enable = mkBooleanOption false "Whether to install the ${name} package";
+    pkg = lib.mkOption {
+      description = "The nixpkg to be used in installing the ${name} package";
+      type = lib.types.package;
+      default = pkg;
+    };
   };
 }
