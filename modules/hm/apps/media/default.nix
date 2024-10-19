24d1aa36e1d32437e4597173ffd5e286d7dafee3
Luna Skye <sepshuncontact@gmail.com>
2024-10-19 17:14:28 -0400

    style(hm/apps): improve media submodule readability

--- modules/hm/apps/media/default.nix
+++ modules/hm/apps/media/default.nix
@@ -10,8 +10,8 @@ in {
     };
 
     video = {
-      vlc = bead.mkPackageOption pkgs.vlc "VLC video player";
-      mpv = bead.mkPackageOption pkgs.mpv "MPV video player";
+      vlc      = bead.mkPackageOption pkgs.vlc      "VLC video player";
+      mpv      = bead.mkPackageOption pkgs.mpv      "MPV video player";
       kdenlive = bead.mkPackageOption pkgs.kdenlive "Kdenlive video editor";
     };
 
@@ -27,9 +27,11 @@ in {
        inherit (lib.lists) optional;
     in 
       optional (cfg.audio.tenacity.enable) cfg.audio.tenacity.pkg ++
+
       optional (cfg.video.vlc.enable)      cfg.video.vlc.pkg      ++
       optional (cfg.video.mpv.enable)      cfg.video.mpv.pkg      ++
       optional (cfg.video.kdenlive.enable) cfg.video.kdenlive.pkg ++
+
       optional (cfg.image.oculante.enable) cfg.image.oculante.pkg ++
       optional (cfg.image.komikku.enable)  cfg.image.komikku.pkg  ++
       []; # its fine, the consistent rows make brain vibrate
