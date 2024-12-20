400722d960844e10966e22202bb5c061770122e9
Luna Skye <sepshuncontact@gmail.com>
2024-12-20 07:39:12 -0500

    refactor(cache): improve option naming

--- modules/cache/nix.nix
+++ modules/cache/nix.nix
@@ -13,9 +13,9 @@ in {
   in {
     enable = bead.mkBooleanOption true "Whether to enable the use of binary caches when building the NixOS system";
 
-    useDefaults = bead.mkBooleanOption true "Whether to use default substituters and trusted keys for caches";
+    useDefaultSubstituters = bead.mkBooleanOption true "Whether to use default substituters and trusted keys for caches";
 
-    extra = bead.mkListOfOption substituterType [] "Extra binary substituters and keys to add to a NixOS host";
+    extraSubstituters = bead.mkListOfOption substituterType [] "Extra binary substituters and keys to add to a NixOS host";
   };
 
 
@@ -26,14 +26,14 @@ in {
       allowed-users = ["@wheel"];
       trusted-users = ["@wheel"];
 
-      substituters = (builtins.map (item: item.url) cfg.extra) ++ optionals (cfg.useDefaults) [
+      substituters = (builtins.map (item: item.url) cfg.extraSubstituters) ++ optionals (cfg.useDefaultSubstituters) [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
         "https://nixpkgs-unfree.cachix.org"
         "https://hyprland.cachix.org"
       ];
 
-      trusted-public-keys = (builtins.map (item: item.key) cfg.extra) ++ optionals (cfg.useDefaults) [
+      trusted-public-keys = (builtins.map (item: item.key) cfg.extraSubstituters) ++ optionals (cfg.useDefaultSubstituters) [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
