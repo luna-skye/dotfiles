5445c5501439bb6934fb7c2cb1847af266cefd99
Luna Skye <sepshuncontact@gmail.com>
2024-12-20 08:00:53 -0500

    fix(session): non overrideable options

--- modules/session/nix.nix
+++ modules/session/nix.nix
@@ -10,15 +10,15 @@ in {
 
 
   config = {
-    services.displayManager.defaultSession = cfg.default;
+    services.displayManager.defaultSession = lib.mkDefault cfg.default;
 
     # TODO: understand what all of these do and if they're still needed, it's old and likely outdated, incorrect, or misplaced
     environment.variables = {
-      NIXOS_OZONE_WL = "1";
-      __GL_GSYNC_ALLOWED = "0";
-      XDG_SESSION_TYPE = "wayland";
+      NIXOS_OZONE_WL = lib.mkDefault "1";
+      __GL_GSYNC_ALLOWED = lib.mkDefault "0";
+      XDG_SESSION_TYPE = lib.mkDefault "wayland";
     };
-    hardware.graphics.enable = true;
-    xdg.portal.wlr.enable = true;
+    hardware.graphics.enable = lib.mkDefault true;
+    xdg.portal.wlr.enable = lib.mkDefault true;
   };
 }
