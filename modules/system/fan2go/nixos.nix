b588f753e1794282acdf785363774c850501a3b6
Luna Skye <sepshuncontact@gmail.com>
2025-09-30 18:58:37 -0400

    feat(system): add fan2go configuration options

--- modules/system/fan2go/nixos.nix
+++ modules/system/fan2go/nixos.nix
@@ -1,8 +1,34 @@
-{ helpers, ... }:
+{ config, helpers, pkgs, lib, ... }:
 
 
-{
+let
+  cfg = config.zen.system.fan2go;
+
+in {
   options.zen.system.fan2go = {
     enable = helpers.mkBooleanOption false "Whether to enable the fan2go fan controlling daemon";
+    autostart = helpers.mkBooleanOption true "Whether to initialize the fan2go daemon on system startup";
+    settings = lib.mkOption {
+      description = ''
+        Fan2Go configuration to write to /etc/fan2go.yaml
+        https://github.com/markusressel/fan2go?tab=readme-ov-file#configuration
+      '';
+      type = lib.types.attrs;
+      default = {};
+    };
+  };
+
+  config = lib.mkIf (cfg.autostart) {
+    systemd.services.fan2go = {
+      description = "fan2go fan controller";
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.fan2go}/bin/fan2go -v";
+        Restart = "always";
+        User = "root";
+      };
+    };
+
+    environment.etc."fan2go/fan2go.yaml".text = lib.generators.toYAML {} ({ api.enabled = true; } // cfg.settings);
   };
 }
