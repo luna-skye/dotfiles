5788b2ff946a668fe7f195a11c9d9d4bf046dafb
Luna Skye <sepshuncontact@gmail.com>
2025-10-15 09:43:58 -0400

    fix(apps/teamspeak): allow insecure package

--- modules/apps/teamspeak/nixos.nix
+++ modules/apps/teamspeak/nixos.nix
@@ -1,8 +1,21 @@
-{ helpers, ... }:
+{ inputs, helpers, lib, config, ... }:
 
 
-{
+let
+  cfg = config.zen.apps.teamspeak;
+  pkgs = import inputs.nixpkgs {
+    system = "x86_64-linux";
+    config.allowUnfree = true;
+    # this bad, fix soon at any cost
+    config.permittedInsecurePackages = [ "qtwebengine-5.15.19" ]; 
+  };
+
+in {
   options.zen.apps.teamspeak = {
-    enable = helpers.mkBooleanOption false "Whether to install the TeamSpeak 6 Client";
+    enable = helpers.mkBooleanOption false "Whether to install the TeamSpeak 3 Client";
+  };
+
+  config = lib.mkIf (cfg.enable) {
+    environment.systemPackages = [ pkgs.teamspeak3 ];
   };
 }
