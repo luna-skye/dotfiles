b84ff9bac435c94ad7bc8ec2ed7aae8a8e5a618a
Luna Skye <sepshuncontact@gmail.com>
2025-12-16 19:16:24 -0500

    feat(services/dms): add stellae theme

--- modules/services/dms/hm.nix
+++ modules/services/dms/hm.nix
@@ -1,9 +1,12 @@
-{ osConfig, config, lib, ... }:
+{ osConfig, config, pkgs, lib, stellae, ... }:
 
 
 let
   hostCfg = osConfig.zen.services.dms;
   cfg = config.zen.services.dms;
+
+  theme = stellae.exporters.dms-shell.plaintext { element = config.zen.theme.element; };
+  themeFile = pkgs.writeText "stellae-dms" theme;
 
 in {
   imports = [];
@@ -11,6 +14,15 @@ in {
   options.zen.services.dms = {};
 
   config = lib.mkIf (hostCfg.enable) {
+    home.file.".config/totality-nixos/stellae-dms-theme.json".text = theme;
 
+    # declarative DMS config, may cause read-only issues
+    # disabled until can put more time into it
+    # home.file.".config/DankMaterialShell/settings.json".text = builtins.toJSON {
+    #   currentThemeName = "custom";
+    #   customThemeFile = themeFile;
+    #   mutagenScheme = "scheme-tonal-spot";
+    #   runUserMatugenTemplates = true;
+    # };
   };
 }
