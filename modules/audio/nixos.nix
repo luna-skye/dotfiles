18cff6f5c66a6d8e704d60fbcc37da63bfc0cbd5
Luna Skye <sepshuncontact@gmail.com>
2025-12-17 16:17:11 -0500

    style(audio): clean this up

--- modules/audio/nixos.nix
+++ modules/audio/nixos.nix
@@ -1,20 +1,26 @@
 { lib, pkgs, ... }:
 
-{
-  imports = [];
+
+let
+  inherit (lib) mkDefault;
+
+in {
   options.zen.audio = {};
+
   config = {
-    security.rtkit.enable = lib.mkDefault true;
-    services.pulseaudio.enable = false;
-    services.pipewire = {
-      enable = lib.mkDefault true;
+    security.rtkit.enable = mkDefault true;
 
-      alsa.enable       = lib.mkDefault true;
-      alsa.support32Bit = lib.mkDefault true;
-
-      pulse.enable = lib.mkDefault true;
-      jack.enable  = lib.mkDefault true;
+    services = {
+      pulseaudio.enable = mkDefault false;
+      pipewire = {
+        enable = mkDefault true;
+        alsa.enable       = mkDefault true;
+        alsa.support32Bit = mkDefault true;
+        pulse.enable      = mkDefault true;
+        jack.enable       = mkDefault true;
+      };
     };
+
     environment.systemPackages = [ pkgs.pulsemixer ];
   };
 }
