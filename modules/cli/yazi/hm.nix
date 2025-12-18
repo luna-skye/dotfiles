3463c1a16c7b261c6b34ed4edfabbf8c505f2048
Luna Skye <sepshuncontact@gmail.com>
2025-12-17 20:30:39 -0500

    fix(cli/yazi): opps

--- modules/cli/yazi/hm.nix
+++ modules/cli/yazi/hm.nix
@@ -35,10 +35,8 @@ in {
         if ya.target_family() ~= "unix" then
           return ""
         end
-
-        return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg(blue)
+        return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
       end, 500, Header.LEFT)
-
 
       -- show user/group of files in status bar
       Status:children_add(function()
@@ -50,7 +48,7 @@ in {
         return ui.Line {
           ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
           ":",
-          ui.Span(ya.user_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+          ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
           " ",
         }
       end, 500, Status.RIGHT)
