246af202599d26bbc89d5125bb78dcdd42e8448e
Luna Skye <sepshuncontact@gmail.com>
2025-12-17 20:22:51 -0500

    feat(cli/yazi): add user/group and user/host

--- modules/cli/yazi/hm.nix
+++ modules/cli/yazi/hm.nix
@@ -28,5 +28,32 @@ in {
       [preview]
       tab_size = 2
     '';
+
+    home.file.".config/yazi/init.lua".text = /* lua */ ''
+      -- show username/hostname in header
+      Header:children_add(function()
+        if ya.target_family() ~= "unix" then
+          return ""
+        end
+
+        return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg(blue)
+      end, 500, Header.LEFT)
+
+
+      -- show user/group of files in status bar
+      Status:children_add(function()
+        local h = cx.active.current.hovered
+        if not h or ya.target_family() ~= "unix" then
+          return ""
+        end
+
+        return ui.Line {
+          ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+          ":",
+          ui.Span(ya.user_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+          " ",
+        }
+      end, 500, Status.RIGHT)
+    '';
   };
 }
