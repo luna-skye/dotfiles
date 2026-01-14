18baccc9ae0c06c10ad9636b2d9f6b7c661b9387
Luna Skye <sepshuncontact@gmail.com>
2026-01-14 18:50:41 -0500

    feat(host): add callisto configuration

---
+++ hosts/callisto/configuration.nix
@@ -0,0 +1,26 @@
+{ ... }: {
+  zen = {
+    host.name = "callisto";
+    system.steamdeck = {
+      enable = true;
+      username = "skye";
+    };
+
+    apps = {
+      zen-browser.enable = true;
+      feishin.enable = true;
+      bottles.enable = true;
+      gaming = {
+        lutris.enable = true;
+        minecraft.prism.enable = true;
+        r2modman.enable = true;
+      };
+    };
+  };
+
+  users.users.skye = {
+    isNormalUser = true;
+    description = "skye";
+    extraGroups = [ "networkmanager" "wheel" "video" "seat" "audio" "libvirtd" ];
+  };
+}
