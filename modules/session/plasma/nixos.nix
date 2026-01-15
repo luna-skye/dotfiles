f1f855f3b14d5ecd924f00a7c20ea2d9049fbcd1
Luna Skye <sepshuncontact@gmail.com>
2026-01-15 02:28:32 -0500

    refactor(session/plasma): option for SDDM enabling

--- modules/session/plasma/nixos.nix
+++ modules/session/plasma/nixos.nix
@@ -9,11 +9,12 @@ in {
 
   options.zen.session.plasma = {
     enable = helpers.mkBooleanOption false "Whether to enable the KDE Plasma Desktop Environment";
+    useSDDM = helpers.mkBooleanOption true "Whether to enable and use the SDDM Display Manager";
   };
 
   config = lib.mkIf (cfg.enable) {
     services.displayManager.sddm = {
-      enable = lib.mkDefault true;
+      enable = lib.mkDefault cfg.useSDDM;
       wayland.enable = lib.mkDefault true;
     };
     services.desktopManager.plasma6.enable = lib.mkDefault true;
