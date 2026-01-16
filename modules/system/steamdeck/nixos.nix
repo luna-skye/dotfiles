c1753b9df20f66721f327e37b305173daa6738ee
Luna Skye <sepshuncontact@gmail.com>
2026-01-16 03:18:01 -0500

    fix(system/steamdeck): steam cef debugging (actually)

--- modules/system/steamdeck/nixos.nix
+++ modules/system/steamdeck/nixos.nix
@@ -110,8 +110,7 @@ in {
     };
 
     # create cef file for decky
-    systemd.tmpfiles.rules = lib.mkIf config.jovian.decky-loader.enable [
-      "d /home/${config.jovian.steam.user}/.local/share/Steam 0755 ${config.jovian.steam.user} users -"
+    systemd.user.tmpfiles.rules = lib.mkIf config.jovian.decky-loader.enable [
       "f /home/${config.jovian.steam.user}/.local/share/Steam/.cef-enable-remote-debugging 0644 ${config.jovian.steam.user} users -"
     ];
   };
