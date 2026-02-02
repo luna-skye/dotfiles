bb76cf578c8c1d679bcb79207525f5da54df6b94
Luna Skye <sepshuncontact@gmail.com>
2026-02-02 11:27:21 -0500

    chore: bump godot version

--- modules/apps/godot/hm.nix
+++ modules/apps/godot/hm.nix
@@ -3,21 +3,17 @@
 let
   hostCfg = osConfig.zen.apps.godot;
 
-  # locked to Godot version 4.2.1
+  # locked to Godot version 4.5.1
   # found using https://history.nix-packages.com/search?search=godot_4
   gdpkgs = import (builtins.fetchGit {
-    name = "godot-4.2.1";
+    name = "godot-4.5.1";
     url = "https://github.com/NixOS/nixpkgs/";
     ref = "refs/heads/nixpkgs-unstable";
-    rev = "02b8c7ddb7fe956871fa65466bf8a30fa69ec078";
+    rev = "5f02c91314c8ba4afe83b256b023756412218535";
   }) { inherit (pkgs.stdenv.hostPlatform) system; };
 
 in {
-  imports = [];
-
-  options.zen.apps.godot = {};
-
   config = lib.mkIf (hostCfg.enable) {
-    home.packages = [ gdpkgs.godot_4 ];
+    home.packages = [ gdpkgs.godot ];
   };
 }
