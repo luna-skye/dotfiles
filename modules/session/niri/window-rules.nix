eb410b73f5c0ae76c833dcb460f58f7b82e8baa0
Luna Skye <sepshuncontact@gmail.com>
2026-02-05 15:40:47 -0500

    refactor(session/niri): window-rules

--- modules/session/niri/window-rules.nix
+++ modules/session/niri/window-rules.nix
@@ -5,8 +5,7 @@ let
   cfg = config.zen.session.niri;
 
 in {
-  options = {};
-
+  options = {}; 
   output = ''
     // Active Window
     window-rule {
@@ -18,19 +17,29 @@ in {
 
     // Application Rules
     window-rule {
-      match app-id="godot"
-      open-maximized true
-      opacity 1.0
-    }
-    window-rule {
-      match app-id="blender"
-      open-maximized true
-      opacity 1.0
-    }
-    window-rule {
       match app-id="zen-twilight"
+      match app-id="blender"
+      match app-id="Godot"
       open-maximized true
       opacity 1.0
+    }
+    window-rule {
+      match title="Open File"
+      match title="Open Folder"
+      match app-id="Godot" title="Create New Node"
+      match app-id="Godot" title="Project Settings"
+      match app-id="Godot" title="Editor Settings"
+      match app-id="Godot" title="Export"
+      match app-id="Godot" title="Run Instances"
+      match app-id="Godot" title="Attach Node Script"
+      match app-id="Godot" title="Select Scene"
+      match app-id="Godot" title="Event Configuration"
+      match app-id="Godot" title="Save"
+      match app-id="Godot" title="Please Confirm"
+      match app-id="Godot" title="Pick Root Node Type"
+      match app-id="Godot" title="(DEBUG)"
+      open-maximized false
+      open-floating true
     }
     window-rule {
       match app-id="kitty"
@@ -48,7 +57,7 @@ in {
       match title="Picture-in-Picture"
       open-focused false
       open-floating true
-      default-floating-position x=32 y=32 relative-to="bottom-right"
+      default-floating-position x=32 y=32 relative-to="bottom-left"
       default-column-width { fixed 480; }
       default-window-height { fixed 270; }
     }
